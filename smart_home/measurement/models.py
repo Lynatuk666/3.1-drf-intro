f3944aff85f6a5d6b2e9c89f184d4a176ca791a2
Lynatuk666 <sanya170498@gmail.com>
2024-11-17 05:22:49 +0300

    initial commit

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Датчик'
+        verbose_name_plural = 'Датчики'
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.temperature} {self.created_at}'
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Измерение'
+        verbose_name_plural = 'Измерения'
--- smart_home/measurement/urls.py
+++ smart_home/measurement/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 
+from .models import Measurement
+from .views import  SensorsView, MeasurementsView, SensorDetailView
+
 urlpatterns = [
+    path('sensors/', SensorsView.as_view()),
+    path('measurements/', MeasurementsView.as_view()),
+    path('sensors/<pk>/', SensorDetailView.as_view()),
     # TODO: зарегистрируйте необходимые маршруты
+
 ]
